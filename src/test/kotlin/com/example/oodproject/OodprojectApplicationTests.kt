14fe294f87166a21630903cadae4c3f717d1ea30
Wasin Wasantivong <chotongza@gmail.com>
2023-02-16 01:14:23 +0700

    first commit

---
+++ src/test/kotlin/com/example/oodproject/OodprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.oodproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OodprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
